1dd762c06a121b132faba29cd859f678efe36ea4
kristjanjansen <kristjan.jansen@gmail.com>
2018-09-30 13:14:51 +0300

    ContentSlides

--- components/ContentSlides.js
+++ components/ContentSlides.js
@@ -6,14 +6,19 @@ export default {
   computed: {
     preparedContent() {
       return this.content
-        .replace(/\n---\n/g, "")
         .replace(/\n--\n/g, "")
-        .replace(/\n-\n/g, "");
+        .replace(/\n-\n/g, "")
+        .split("---");
     }
   },
   template: `
     <div>
-      <Markdown :content="preparedContent"/>
+      <div
+        v-for="slide in preparedContent"
+        style="border: 3px solid black"
+      >
+        <Markdown :content="slide"/>
+      </div>
     </div>
   `
 };
--- content/index.js
+++ content/index.js
@@ -1,4 +1,6 @@
+import Buttons from "../components/Buttons.js";
 import ContentDocument from "../components/ContentDocument.js";
+import ContentSlides from "../components/ContentSlides.js";
 import AdvancedEditor from "../components/AdvancedEditor.js";
 import Editor from "../components/Editor.js";
 
@@ -6,19 +8,33 @@ import sample from "./sample.js";
 
 new Vue({
   el: "#app",
-  components: { Editor, AdvancedEditor, ContentDocument },
-  data: () => ({ content: sample }),
+  components: {
+    Editor,
+    AdvancedEditor,
+    ContentDocument,
+    ContentSlides,
+    Buttons
+  },
+  data: () => ({
+    content: sample,
+    components: [
+      { title: "Document", component: ContentDocument },
+      { title: "Slides", component: ContentSlides }
+    ],
+    activeComponent: 0
+  }),
   template: `
     <div>
       <header>
         Content
+        <Buttons :buttons="components.map(c => c.title)" v-model="activeComponent"/>
       </header>
       <main style="height: calc(100vh - 5rem)">
         <section>
           <Editor v-model="content" />
         </section>
         <section style="flex: 1; padding: 2rem;">
-          <ContentDocument :content="content" />
+          <component :is="components[activeComponent].component" :content="content" />
         </section>
       </main>
     </div>
