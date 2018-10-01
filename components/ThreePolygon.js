ef0ddf6b8aa498453ba2ee551a93415dbd8b1ec7
kristjanjansen <kristjan.jansen@gmail.com>
2018-10-01 23:15:09 +0300

    ok

--- components/ThreePolygon.js
+++ components/ThreePolygon.js
@@ -8,7 +8,7 @@ const InternalThreePolygon = {
   data() {
     let curObj = this.obj;
     if (!curObj) {
-      var vectorPoints = this.points.map(p => new THREE.Vector2(p.x, p.y));
+      var vectorPoints = this.points.map(p => new THREE.Vector3(p.x || 0, p.y || 0, p.z || 0));
       var shape = new THREE.Shape(vectorPoints);
       var geometry = new THREE.ShapeGeometry(shape);
       curObj = new THREE.Mesh(
@@ -20,6 +20,30 @@ const InternalThreePolygon = {
     return { curObj };
   }
 };
+
+//import { Object3D } from "./internal/three.js";
+
+// const InternalThreePolygon = {
+//   mixins: [Object3D],
+//   props: { points: { default: [] } },
+//   data() {
+//     let curObj = this.obj;
+//     if (!curObj) {
+//       var geometry = new THREE.Geometry();
+//       this.points.forEach(p => {
+//         geometry.vertices.push(new THREE.Vector3(p.x || 0, p.y || 0, p.z || 0));
+//       });
+//       geometry.faces.push(new THREE.Face3(0, 1, 2));
+//       geometry.computeFaceNormals();
+//       curObj = new THREE.Mesh(
+//         geometry,
+//         new THREE.MeshNormalMaterial({ opacity: 0.5, side: THREE.DoubleSide })
+//       );
+//     }
+//     curObj.name = curObj.name || curObj.type;
+//     return { curObj };
+//   }
+// };
 
 export default {
   name: "ThreePolygon",
@@ -36,10 +60,18 @@ export default {
   `,
   components: { InternalThreePolygon, ThreeGroup, ThreeLine },
   props: { points: { default: [] } },
+  computed: {
+    linePoints() {
+      return this.points.concat(this.points[0]).map(p => {
+        p.z = 0
+        return p
+      })
+    }
+  },
   template: `
     <ThreeGroup>
       <InternalThreePolygon :points="points" />
-      <ThreeLine :points="points.concat(points[0])" />
+      <ThreeLine :points="linePoints" />
     </ThreeGroup>
   `
 };
