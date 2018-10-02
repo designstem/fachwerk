fec21da44c58589059d46aee1cf03ee270f0f090
kristjanjansen <kristjan.jansen@gmail.com>
2018-10-02 16:47:45 +0300

    mouse

--- components/TwoScene.js
+++ components/TwoScene.js
@@ -15,10 +15,25 @@ Note that **Y axis is flipped** and <code>x</code> and <code>y</code> span from 
   </Anime>
 </TwoScene>
   `,
-  props: { size: { default: 250 } },
+  props: { size: { default: 250 }, mouse: { default: false } },
   computed: {
     viewBox() {
       return `-2 -2 4 4`;
+    }
+  },
+  data: () => ({ mouseX: 0, mouseY: 0 }),
+  methods: {
+    onSceneMousemove(e) {
+      let svg = this.$refs.svg;
+      let point = svg.createSVGPoint()
+      point.x = e.clientX
+      point.y = e.clientY
+      let ctm = e.target.getScreenCTM();
+      if (ctm = ctm.inverse()) {
+        point = point.matrixTransform(ctm)
+      }
+      this.mouseX = point.x
+      this.mouseY = point.y
     }
   },
   template: `
@@ -27,10 +42,13 @@ Note that **Y axis is flipped** and <code>x</code> and <code>y</code> span from 
         :height="size"
         :view-box.camel="viewBox"
         class="two"
+        @mousemove="onSceneMousemove"
+        ref="svg"
     >
       <g transform="scale(1,-1)">
         <slot />
       </g>
+      <circle v-if="mouse" :cx="mouseX" :cy="mouseY" r="0.1" />
     </svg>
   `,
   css: `
