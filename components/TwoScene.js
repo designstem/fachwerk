81bb64a1a198b4eb9950793e7b4d32f08e183d0e
kristjanjansen <kristjan.jansen@gmail.com>
2018-10-02 17:52:12 +0300

    fix

--- components/TwoScene.js
+++ components/TwoScene.js
@@ -17,7 +17,7 @@ Note that **Y axis is flipped** and <code>x</code> and <code>y</code> span from 
     slot-scope="{ value }"
     :cx="value.mouseX"
     :cy="value.mouseY"
-    r="0.1"
+    :r="value.mousePressed ? 0.2 : 0.1"
     fill="var(--color-red)"
   />
 </TwoScene>
@@ -28,7 +28,7 @@ Note that **Y axis is flipped** and <code>x</code> and <code>y</code> span from 
       return `-2 -2 4 4`;
     }
   },
-  data: () => ({ mouseX: 0, mouseY: 0 }),
+  data: () => ({ mouseX: 0, mouseY: 0, mousePressed: false }),
   methods: {
     onSceneMousemove(e) {
       let svg = this.$refs.svg;
@@ -51,11 +51,13 @@ Note that **Y axis is flipped** and <code>x</code> and <code>y</code> span from 
         :view-box.camel="viewBox"
         class="two"
         @mousemove="onSceneMousemove"
+        @mousedown="mousePressed = true"
+        @mouseup="mousePressed = false"
         ref="svg"
     >
       <g transform="scale(1,-1)" ref="container">
         <slot name="content" />
-        <slot :value="{ mouseX, mouseY }" />
+        <slot :value="{ mouseX, mouseY, mousePressed }" />
       </g>
     </svg>
   `,
