cca1be92834e8516bfe229260ae31a62789f4ef2
kristjanjansen <kristjan.jansen@gmail.com>
2018-10-02 17:47:07 +0300

    fix

--- components/TwoScene.js
+++ components/TwoScene.js
@@ -10,12 +10,19 @@ Note that **Y axis is flipped** and <code>x</code> and <code>y</code> span from 
 `,
   example: `
 <TwoScene>
-  <Anime :to="1" :alternate="true">
-    <circle slot-scope={value} :r="value" />
-  </Anime>
+  <g slot="content">
+    <circle r="1" />
+  </g>  
+  <circle
+    slot-scope="{ value }"
+    :cx="value.mouseX"
+    :cy="value.mouseY"
+    r="0.1"
+    fill="var(--color-red)"
+  />
 </TwoScene>
   `,
-  props: { size: { default: 250 }, mouse: { default: false } },
+  props: { size: { default: 250 } },
   computed: {
     viewBox() {
       return `-2 -2 4 4`;
@@ -25,15 +32,16 @@ Note that **Y axis is flipped** and <code>x</code> and <code>y</code> span from 
   methods: {
     onSceneMousemove(e) {
       let svg = this.$refs.svg;
-      let point = svg.createSVGPoint()
-      point.x = e.clientX
-      point.y = e.clientY
-      let ctm = e.target.getScreenCTM();
-      if (ctm = ctm.inverse()) {
-        point = point.matrixTransform(ctm)
+      let point = svg.createSVGPoint();
+      point.x = e.clientX;
+      point.y = e.clientY;
+      //let ctm = e.target.getScreenCTM();
+      let ctm = this.$refs.container.getScreenCTM();
+      if ((ctm = ctm.inverse())) {
+        point = point.matrixTransform(ctm);
       }
-      this.mouseX = point.x
-      this.mouseY = point.y
+      this.mouseX = point.x;
+      this.mouseY = point.y;
     }
   },
   template: `
@@ -45,10 +53,10 @@ Note that **Y axis is flipped** and <code>x</code> and <code>y</code> span from 
         @mousemove="onSceneMousemove"
         ref="svg"
     >
-      <g transform="scale(1,-1)">
-        <slot />
+      <g transform="scale(1,-1)" ref="container">
+        <slot name="content" />
+        <slot :value="{ mouseX, mouseY }" />
       </g>
-      <circle v-if="mouse" :cx="mouseX" :cy="mouseY" r="0.1" />
     </svg>
   `,
   css: `
