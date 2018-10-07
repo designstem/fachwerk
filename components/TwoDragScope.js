ca500fc7b346c322c33419e03c8fdecdb79fd6fd
kristjanjansen <kristjan.jansen@gmail.com>
2018-10-07 16:00:48 +0300

    fix

--- components/TwoDragScope.js
+++ components/TwoDragScope.js
@@ -4,42 +4,43 @@ import { snapToGrid } from "../utils.js"
 export default {
   description: `
 **🔬 This component is experimental.**
+<br><br><br><br><br><br>
 `,
   example: `
 <TwoSceneScope>  
   <TwoDragScope
-    slot-scope="mouseData"
-    :mouseData="mouseData"
+    slot-scope="data"
+    :mouse="data.mouse"
     :points="[
-      { x: 0, y: 1 },
-      { x: 1, y: -1 },
+      { x:  0, y:  1 },
+      { x:  1, y: -1 },
       { x: -1, y: -1 }
     ]"
     :snap="true"
   >
     <TwoGroup slot-scope="dragData">
       <TwoGrid />
-      <TwoPolygon :points="dragData.dragData" />
+      <TwoPolygon :points="dragData.draggedPoints" />
     </TwoGroup> 
   </TwoDragScope>
 </TwoSceneScope>
   `,
-  props: ['mouseData', 'points', 'snap' ],
+  props: ['mouse', 'points', 'snap' ],
   data: function() {
-    return { draggablePoints: this.points }
+    return { draggedPoints: this.points }
   },
   methods: {
     handleMove(p) {
       if (p.pressed) {
-        p.x = this.snap ? snapToGrid(this.mouseData.value.mouseX, 0.25) : this.mouseData.value.mouseX
-        p.y = this.snap ? snapToGrid(this.mouseData.value.mouseY, 0.25) : this.mouseData.value.mouseY
+        p.x = this.snap ? snapToGrid(this.mouse.x, 0.25) : this.mouse.x
+        p.y = this.snap ? snapToGrid(this.mouse.y, 0.25) : this.mouse.y
       }
     }
   },
   template: `
     <g>
-      <slot :dragData="draggablePoints" />
-      <g v-for="(p,i) in draggablePoints">
+      <slot :draggedPoints="draggedPoints" />
+      <g v-for="(p,i) in draggedPoints">
         <circle
           :cx="p.x"
           :cy="p.y"
