3831189645ab490ca28415dd2140b405140b7a0c
kristjanjansen <kristjan.jansen@gmail.com>
2018-10-08 16:44:33 +0300

    fix

--- content/experimentssample.js
+++ content/experimentssample.js
@@ -17,9 +17,9 @@ Inspired by https://kottke.org/17/11/classical-music-scores-as-colorful-data-vis
         scale(r,0,20,0.5,1.5)
       )"
       :r="any(
-        random(0.05,0.1),
-        random(0.05,0.1),
-        random(0.1,0.15)
+        random(0.05,0.1,true),
+        random(0.05,0.1,true),
+        random(0.1,0.15,true)
       )"
       :fill="hsl(random(160,360),100,50,0.25)"
     />
