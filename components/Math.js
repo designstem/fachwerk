387ded8fe05c38c2e9724116c718cc1c381d274c
kristjanjansen <kristjan.jansen@gmail.com>
2018-10-09 14:02:17 +0300

    fix

--- components/Math.js
+++ components/Math.js
@@ -31,8 +31,7 @@ Updates when edited and slider changed
   methods: {
     renderMath() {
       this.math = katex.renderToString(
-        //this.$slots.default[0].text.trim().replace(/\n+/g, "\\newline"),
-        this.$slots.default[0].text.trim(),
+        this.$slots.default[0].text.trim().replace(/\n+/g, "\\newline"),
         {
           throwOnError: false
         }
