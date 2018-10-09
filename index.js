a60efa6cb12b8f334c1bb7367d7fa2ffe0839c42
kristjanjansen <kristjan.jansen@gmail.com>
2018-10-09 14:46:32 +0300

    fix

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ import Components from "./components/internal/Components.js";
 import Utils from "./components/internal/Utils.js";
 import Faq from "./components/internal/Faq.js";
 
-// Vue.config.warnHandler = function(msg, vm, trace) {
-// };
-
 new Vue({
   components: { Components, Utils, Faq },
   el: "#app",
