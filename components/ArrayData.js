3fb40af91c9987c96299f5e45e9064d1221c3ec3
kristjanjansen <kristjan.jansen@gmail.com>
2018-10-25 22:36:54 +0300

    dimensions

--- components/ArrayData.js
+++ components/ArrayData.js
@@ -1,11 +1,35 @@
 export default {
   methods: {
-    onUpdate(i, newValue) {
-      this.value[i] = newValue;
-      this.$set(this.value, i, newValue);
+    onUpdate(newValue, x, y = -1, z = -1) {
+      // if (y > -1 && z > -1) {
+      //   this.$set(this.value, x, newValue);
+      // }
+      if (y > -1 && z == -1) {
+        this.$set(this.value[x], y, newValue);
+      }
+      if (y == -1 && z == -1) {
+        this.$set(this.value, x, newValue);
+      }
+
     }
   },
   example: `
+<ArrayData :length="3" :dimensions="2">
+  <TwoScene slot-scope="data">
+  <template v-for="(x,i) in data.value">
+  <circle
+      v-for="(y,j) in x"
+      :key="i * j"
+      :cx="i - 1"
+      :cy="j - 1"
+      r="0.5"
+      :fill="y ? 'var(--red)' : 'var(--primary)'"
+      @click="data.update(1 - y, i, j)"
+    />
+  </template>
+  </TwoScene>
+</ArrayData>
+
 <ArrayData :length="3">
   <TwoScene slot-scope="data">
     <circle
@@ -13,10 +37,8 @@ export default {
       :key="i"
       :cx="i - 1"
       r="0.5"
-      :fill="
-      data.value[i] ? 'var(--red)' : 'var(--primary)'
-      "
-      @click="data.update(i, 1 - data.value[i])"
+      :fill="x ? 'var(--red)' : 'var(--primary)'"
+      @click="data.update(1 - x, i)"
     />
   </TwoScene>
 </ArrayData>
@@ -26,7 +48,21 @@ export default {
     dimensions: { default: 1, type: Number }
   },
   created() {
-    this.value = Array(this.length).fill(0)
+    if (this.dimensions == 3) {
+      this.value = Array.from({ length: this.length }).map(_ =>
+        Array.from({ length: this.length }).map(_ =>
+          Array.from({ length: this.length }).map(_ => 0)
+        )
+      );
+    }
+    if (this.dimensions == 2) {
+      this.value = Array.from({ length: this.length }).map(_ =>
+        Array.from({ length: this.length }).map(_ => 0)
+      );
+    }
+    if (this.dimensions == 1) {
+      this.value = Array.from({ length: this.length }).map(_ => 0);
+    }
   },
   data: () => ({ value: [] }),
   template: `
