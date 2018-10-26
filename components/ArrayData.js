a1904720d7932ee676f7757182217c06e14e85c4
kristjanjansen <kristjan.jansen@gmail.com>
2018-10-26 18:34:24 +0300

    better example

--- components/ArrayData.js
+++ components/ArrayData.js
@@ -14,22 +14,6 @@ export default {
     }
   },
   example: `
-<ArrayData :length="3" :dimensions="2">
-  <TwoScene slot-scope="data">
-  <template v-for="(col, x) in data.value">
-  <circle
-    v-for="(value, y) in col"
-    :key="x * y"
-    :cx="x - 1"
-    :cy="y - 1"
-    r="0.5"
-    :fill="value ? 'var(--red)' : 'var(--primary)'"
-    @click="data.update(1 - value, x, y)"
-  />
-  </template>
-  </TwoScene>
-</ArrayData>
-
 <ArrayData :length="3">
   <TwoScene slot-scope="data">
   <circle
@@ -40,6 +24,23 @@ export default {
     :fill="value ? 'var(--red)' : 'var(--primary)'"
     @click="data.update(1 - value, x)"
   />
+  </TwoScene>
+</ArrayData>
+
+<ArrayData :length="7" :dimensions="2">
+  <TwoScene slot-scope="data">
+  <template v-for="(col, x) in data.value">
+  <two-box
+    v-for="(value, y) in col"
+    :key="x * y"
+    :x="x / 2 - 1.2"
+    :y="y / 2 - 1.2"
+    width="0.49"
+    height="0.49"
+    :fill="value ? 'var(--red)' : 'var(--primary)'"
+    @click.native="data.update(1 - value, x, y)"
+  />
+  </template>
   </TwoScene>
 </ArrayData>
   `,
