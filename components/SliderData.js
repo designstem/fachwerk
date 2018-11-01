77918215549b853443b62b2e7c90f00f1d4d8ba7
kristjanjansen <kristjan.jansen@gmail.com>
2018-11-01 18:56:07 +0200

    sliderdata values

--- components/SliderData.js
+++ components/SliderData.js
@@ -9,10 +9,26 @@ Adds a slider next to the content and passing the slider value as <code>data.val
   </h1>
 </SliderData>
 
-<SliderData :from="10" :to="20" :value="15">
-  <h1 class="bullet" slot-scope="data">
-    {{ data.value }}
-  </h1>
+<SliderData :values="[
+  { title: 'R', to: 255, value: 102 },
+  { title: 'G', to: 255, value: 51 },
+  { title: 'B', to: 255, value: 153 },
+  { title: 'A', to: 1, value: 1, float: true },
+]">
+  <TwoScene slot-scope="data">
+    <TwoCircle :fill="rgb(...data.values)" />
+  </TwoScene>
+</SliderData>
+
+<SliderData :values="[
+  { title: 'H', to: 255, value: 270 },
+  { title: 'S', to: 100, value: 50 },
+  { title: 'L', to: 100, value: 40 },
+  { title: 'A', to: 1, value: 1, float: true },
+]">
+  <TwoScene slot-scope="data">
+    <TwoCircle :fill="hsl(...data.values)" />
+  </TwoScene>
 </SliderData>
   `,
   props: {
@@ -21,15 +37,36 @@ Adds a slider next to the content and passing the slider value as <code>data.val
     from: { default: 0, type: Number },
     to: { default: 100, type: Number },
     step: { default: 1, type: Number },
+    values: { default: () => [], type: [Array] },
+    float: { default: false, type: Boolean }
   },
   data: function() {
-    return { innerValue: this.value };
+    return {
+      innerValue: this.value,
+      innerValues: this.values.length
+        ? this.values.map(v => (v.value ? v.value : 0))
+        : null
+    };
   },
   template: `
     <div>
-      <label>{{ title }} <code>{{ innerValue }}</code></label>
-      <input style="margin-bottom: 1rem;" type="range" v-model="innerValue" :min="from" :max="to" :step="step" />
-      <slot :value="innerValue"/>
+      <div v-if="values.length" v-for="(v,i) in values" :key="i">
+        <label>{{ v.title ? v.title : 'Values ' + i }} <code>{{ innerValues[i] }}</code></label>
+        <input
+          style="margin-bottom: 1rem;"
+          type="range"
+          v-model="innerValues[i]"
+          :min="v.from"
+          :max="v.to"
+          :step="v.step ? v.step : v.float ? 0.01 : 1"
+        />
+      </div>
+      <slot v-if="values.length" :values="innerValues" />
+      <div v-if="!values.length">
+        <label>{{ title }} <code>{{ innerValue }}</code></label>
+        <input style="margin-bottom: 1rem;" type="range" v-model="innerValue" :min="from" :max="to" :step="step ? step : float ? 0.01 : 1" />
+      </div>
+      <slot v-if="!values.length" :value="innerValue" />
     </div>
   `
 };
