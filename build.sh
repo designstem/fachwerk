8e77e412e22f7ae7f7ce19bc07b6952587cb40f9
kristjanjansen <kristjan.jansen@gmail.com>
2018-11-08 07:52:20 +0200

    with aframe

--- build.sh
+++ build.sh
@@ -6,9 +6,10 @@ rm -f vendor.css
 curl -L https://unpkg.com/katex/dist/katex.min.css >> vendor.css
 rm -f vendor.js
 curl -L https://unpkg.com/vue/dist/vue.min.js >> vendor.js
-curl -L https://unpkg.com/three/build/three.min.js >> vendor.js
 curl -L https://unpkg.com/katex/dist/katex.min.js >> vendor.js
 curl -L https://unpkg.com/marked/marked.min.js >> vendor.js
 curl -L https://unpkg.com/animejs/anime.min.js >> vendor.js
 curl -L https://unpkg.com/d3 >> vendor.js
+# curl -L https://unpkg.com/three/build/three.min.js >> vendor.js
+curl -L https://unpkg.com/aframe | npx uglifyjs >> vendor.js
 cat ./three.js | npx uglifyjs >> vendor.js
