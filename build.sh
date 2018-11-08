49e0639b1d70928e33882d0b2a93d2a702c1bd4c
kristjanjansen <kristjan.jansen@gmail.com>
2018-11-08 07:44:47 +0200

    vendor css

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 npx rollup -f iife -i index.js | npx uglifyjs > build.js
 
-rm vendor.js
+rm -f vendor.css
+curl -L https://unpkg.com/katex/dist/katex.min.css >> vendor.css
+rm -f vendor.js
 curl -L https://unpkg.com/vue/dist/vue.min.js >> vendor.js
 curl -L https://unpkg.com/three/build/three.min.js >> vendor.js
 curl -L https://unpkg.com/katex/dist/katex.min.js >> vendor.js
