e272d1406c21889f69a5c33ade2a9e04dfd57f1c
kristjanjansen <kristjan.jansen@gmail.com>
2018-11-09 23:47:32 +0200

    fix

--- components/layout/FContentEditor.js
+++ components/layout/FContentEditor.js
@@ -70,7 +70,7 @@ export default {
     min-height: 100vh;
   }
   .f-content-editor > div:nth-child(1) {
-    width: 40vw;
+    width: 45vw;
   }
   @media (max-width: 800px) {
     .f-content-editor {
