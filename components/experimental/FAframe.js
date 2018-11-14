eda4cd76e0a19bc85e61ebafabbdc52ef71a83df
kristjanjansen <kristjan.jansen@gmail.com>
2018-11-14 18:28:52 +0100

    vr fixes

--- components/experimental/FAframe.js
+++ components/experimental/FAframe.js
@@ -6,15 +6,15 @@ export default {
 A basic [A-Frame](https://aframe.io/) wrapper.
   `,
   props: {
-    width: { default: 250, type: Number },
-    height: { default: 250, type: Number },
-    color: { default: '#111', type: Number },
+    width: { default: 250, type: [Number,String] },
+    height: { default: 250, type: [Number,String] },
+    color: { default: '#111', type: String },
     timeout: { default: 700, type: Number }
   },
   template: `
     <a-scene embedded :style="{
-      width: width + 'px',
-      height: height + 'px'
+      width: typeof width == 'number' ? width + 'px' : width,
+      height: typeof height == 'number' ? height + 'px' : height,
     }">
       <a-camera>
         <a-cursor
