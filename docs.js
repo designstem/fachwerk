0ff2ad0e9408c939e7bb41d2f817496c69c88da1
kristjanjansen <kristjan.jansen@gmail.com>
2019-01-04 20:08:06 +0200

    fix

--- docs.js
+++ docs.js
@@ -1,15 +1,6 @@
 import * as components from "./framework.js";
 import { sortedComponents } from "./framework.js";
 import { kebabCase } from "./utils.js";
-import { docs } from '../utils.js'
-
-// export default {
-//   components: { Markdown },
-//   data: () => ({ docs: docs() }),
-//   template: `
-//       <Markdown :content="docs.join('<br><p />')" />
-//   `
-// }
 
 import Init from "./components/Init.js";
 
