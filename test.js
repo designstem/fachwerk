3da549c312bcfeb201a02179f746bc9fb6933d00
kristjanjansen <kristjan.jansen@gmail.com>
2019-01-18 23:27:37 +0200

    committing initial test suite

---
+++ test.js
@@ -0,0 +1,54 @@
+// npm i
+// node -r esm test.js
+// node -r esm test.js test_name_here
+
+import * as tests from "./test/tests.js";
+import { equal } from './test/utils.js'
+
+const reset = "\x1b[0m";
+const red = "\x1b[31m";
+const redbg = "\x1b[41m";
+const green = "\x1b[32m";
+const greenbg = "\x1b[42m";
+const dim = "\x1b[2m";
+
+console.log(`\n  ${dim}Running Fachwerk tests${reset}\n`);
+
+const shorten = (str,length = 50, suffix = '...') => 
+  `${str.slice(0, length)}${str.length - 1 > length ? suffix : '' }`
+
+const filteredKey = process.argv[2]
+
+let passed = 0
+let failed = 0
+
+Object.keys(tests).filter(key => filteredKey ? key === filteredKey : true).forEach(key => {
+  const [source, target] = tests[key]();
+  if (equal(source, target)) {
+    passed++
+    console.log(`  ${shorten(key).padEnd(53)}\t${green}OK${reset}`);
+  } else {
+    failed++
+    console.log(`  ${shorten(key).padEnd(53)}\t${red}Failed${reset}${dim}
+ 
+${String(tests[key]).split('\n').map(row => `    ${row}`).join('\n')}
+
+    Source: ${reset}${red}${JSON.stringify(source)}${reset}${dim}
+
+    Target: ${JSON.stringify(target)}
+${reset}`);
+  }
+});
+
+if (passed || failed) {
+console.log(`
+
+  ${greenbg} Tests passed: ${passed} ${reset} ${ failed ? `${redbg} Tests failed: ${failed} ${reset}` : ''}
+
+`)
+} else {
+console.log(`
+  No tests found
+
+`)
+}
---
+++ test/utils.js
@@ -0,0 +1,160 @@
+export const positionTransform = coords => {
+  const c = parseCoords(coords)
+  return `translate(${c[0][0]} ${c[0][1]})`
+}
+
+export const rotationTransform = coords => {
+  const c = parseCoords(coords)
+  return `rotate(${c[0][0]}deg)`;
+}
+
+export const scaleTransform = coords => {
+  const c = parseCoords(coords, normalizeScale)
+  return `scale(${c[0][0]} ${c[0][1]})`;
+}
+
+const padArrayRight = (arr,length,fill) => {
+  return [...arr, ...Array(length).fill(fill)].slice(0,length)
+}
+
+const isObject = object => typeof object == "object" && !Array.isArray(object)
+
+export const normalizeDefault = arr => 
+  padArrayRight(arr, 3, 0).map(value => makeNumber(value))
+
+
+export const normalizeScale = arr => {
+  if (arr.length == 1) {
+    return [arr[0],arr[0],arr[0]]
+  }
+  return normalizeDefault(arr)  
+
+
+
+}
+
+export const coordsTextToArray = (text,normalizer) => {
+  if (text.length == 0) {
+    return coordsTextToArray('0', normalizer)
+  }
+  return text.split(",").map(t =>
+    t
+      .trim()
+      .replace(/\s+/g, " ")
+      .split(" ")
+      .map(value => makeNumber(value))
+  ).map(normalizer)
+};
+
+export const coordsNumberToArray = (number, normalizer) => {
+  return [normalizer([number])]
+}
+
+export const coordsArrayToArray = (arr,normalizer) => {
+
+ const coords = arr.map(a => {
+  if (Array.isArray(a)) {
+     return normalizer(a)
+   }
+  if (typeof a == 'string') {
+     return coordsTextToArray(a, normalizer)[0]
+   }
+  if (typeof a == 'number') {
+    return coordsNumberToArray(a, normalizer)[0]
+  }
+  if (isObject(a)) {
+    return coordsObjectToArray(a, normalizer)[0]
+  }
+  // TODO Fail silently?
+  return a
+ })
+ if (Array.isArray(coords[0])) {
+   return coords
+ }
+ return [normalizer(coords)]
+};
+
+export const coordsObjectToArray = (obj, normalizer = normalizeDefault) => {
+
+  if (obj.hasOwnProperty('x') && obj.hasOwnProperty('y') && obj.hasOwnProperty('z')) {
+    return [normalizer([obj.x, obj.y, obj.z])]
+  }
+  if (obj.hasOwnProperty('x') && obj.hasOwnProperty('y') && !obj.hasOwnProperty('z')) {
+    return [normalizer([obj.x, obj.y])]
+  }
+  if (obj.hasOwnProperty('x') && !obj.hasOwnProperty('y') && !obj.hasOwnProperty('z')) {
+    return [normalizer([obj.x])]
+  }
+  return [normalizer([])]
+}
+
+export const makeNumber = (value) => {
+  const float = parseFloat(value)
+  if (isNaN(float)) {
+    return 0
+  }
+  return float
+}
+
+// const coordsArrayToObject = array => {
+//   return array.map(a => ({ x: a[0], y: a[1] || 0, z: a[2] || 0 })).map(arr => padArrayRight(arr, 3, 0));
+// };
+
+// const coordsObjectToObject = a => {
+//   return { x: a.x || 0, y: a.y || 0, z: a.z || 0 };
+// };
+
+export const parseCoords = (c, normalizer = normalizeDefault) => {
+  if (typeof c == "string") {
+    return coordsTextToArray(c, normalizer);
+  }
+  if (typeof c == "number") {
+    return coordsNumberToArray(c, normalizer);
+  }
+  if (Array.isArray(c)) {
+    return coordsArrayToArray(c, normalizer);
+  }
+  if (isObject(c)) {
+    return coordsObjectToArray(c, normalizer)
+  }
+  return null;
+};
+
+// export const parseCoordsOld = c => {
+//   if (typeof c == "string") {
+//     return coordsArrayToObject(coordsTextToArray(c));
+//   }
+//   if (Array.isArray(c)) {
+//     return normalizeArray(c);
+//   }
+//   if (!Array.isArray(c) && typeof c == "object") {
+//     return c;
+//   }
+//   return null;
+// };
+
+export const equal = (c,d) => {
+  const seen = new Map();
+  return (function compare(a,b) {
+    if (Object.is(a, b)) {
+      return true;
+    }
+    if (a && typeof a === "object" && b && typeof b === "object") {
+      if (seen.get(a) === b) {
+        return true;
+      }
+      if (Object.keys(a || {}).length !== Object.keys(b || {}).length) {
+        return false;
+      }
+      const merged = { ...a, ...b };
+      for (const key in merged) {
+        if (!compare(a && a[key], b && b[key])) {
+          return false;
+        }
+      }
+      seen.set(a, b);
+      return true;
+    }
+    return false;
+  })(c, d);
+}
