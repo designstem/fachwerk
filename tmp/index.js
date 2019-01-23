34bbd7df6889aa31b21d449ed1596cf02c0f7ae7
kristjanjansen <kristjan.jansen@gmail.com>
2019-01-23 11:10:13 +0200

    fixing triangle

--- tmp/index.js
+++ tmp/index.js
@@ -15,9 +15,17 @@ new Vue({
   <f-scene3>
     <f-group3 rotation="10 10">
       <f-grid3 />
-      <f-point3
-        points="0 0; 1 1"
-      />
+      <f-triangle3
+        x1="1"
+        y1="1"
+        z1="0"
+        x2="1"
+        y2="0"
+        z2="1"
+        x3="1"
+        y3="-1"
+        z3="0"
+    /> 
     </f-group3>
   </f-scene3>
   <f-scene grid>
