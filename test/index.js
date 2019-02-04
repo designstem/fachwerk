2070ae19811bfcb6b15e5e4eaade09162900159f
kristjanjansen <kristjan.jansen@gmail.com>
2019-02-04 10:54:28 +0200

    improving tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,9 @@
 import { test } from "../utils.js";
 import * as tests from "./tests.js";
 
-const filteredTests = Object.entries(tests).filter(([name, test]) => name.startsWith('hsl'))
+const url = new URL(window.location.href);
+const filter = url.hash ? url.hash.replace('#','') : null
+
+const filteredTests = Object.entries(tests).filter(([name, test]) => filter ? name.startsWith(filter) : true)
 
 test(filteredTests)
