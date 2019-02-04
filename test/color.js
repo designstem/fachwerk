d79f6961d0a6a2ad04bb6260563f170b7d047216
kristjanjansen <kristjan.jansen@gmail.com>
2019-02-04 10:06:20 +0200

    general cleanup

--- test/color.js
+++ test/color.js
@@ -1,6 +1,7 @@
 import { rgb, rgb2hsl, hsl, hsl2rgb, colorblind, contrast } from "../utils.js";
+import { color } from "../src/utils/color.js";
 
-// Css formatting
+// RGB formatting
 
 export const rgb_to_css = () => {
   const actual = rgb(255, 0, 0);
@@ -14,31 +15,7 @@ export const rgba_to_css = () => {
   return [expected, actual];
 };
 
-export const hsl_to_css = () => {
-  const actual = hsl(360, 0, 0);
-  const expected = "hsl(360,0%,0%)";
-  return [expected, actual];
-};
-
-export const hsl_default_l_to_css = () => {
-  const actual = hsl(360, 0);
-  const expected = "hsl(360,0%,50%)";
-  return [expected, actual];
-};
-
-export const hsl_default_s_l_to_css = () => {
-  const actual = hsl(360);
-  const expected = "hsl(360,100%,50%)";
-  return [expected, actual];
-};
-
-export const hsla_to_css = () => {
-  const actual = hsl(360, 0, 0, 0.5);
-  const expected = "hsla(360,0%,0%,0.5)";
-  return [expected, actual];
-};
-
-// Conversions
+// RGB Conversions
 
 export const rgb_to_hsl_browser = () => {
   const actual = rgb2hsl(255, 0, 0);
@@ -64,6 +41,40 @@ export const rgba_string_to_hsl_browser = () => {
   return [expected, actual];
 };
 
+export const rgba_to_hsl_array_browser = () => {
+  const actual = rgb2hsl(255, 0, 0, 0.5, true);
+  const expected = [0,100,50,0.5];
+  return [expected, actual];
+};
+
+// HSL formatting
+
+export const hsl_to_css = () => {
+  const actual = hsl(360, 0, 0);
+  const expected = "hsl(360,0%,0%)";
+  return [expected, actual];
+};
+
+export const hsl_default_l_to_css = () => {
+  const actual = hsl(360, 0);
+  const expected = "hsl(360,0%,50%)";
+  return [expected, actual];
+};
+
+export const hsl_default_s_l_to_css = () => {
+  const actual = hsl(360);
+  const expected = "hsl(360,100%,50%)";
+  return [expected, actual];
+};
+
+export const hsla_to_css = () => {
+  const actual = hsl(360, 0, 0, 0.5);
+  const expected = "hsla(360,0%,0%,0.5)";
+  return [expected, actual];
+};
+
+// HSL conversion
+
 export const hsl_to_rgb_browser = () => {
   const actual = hsl2rgb(0, 100, 50);
   const expected = "rgb(255,0,0)";
@@ -85,6 +96,12 @@ export const hsl_string_to_rgb_browser = () => {
 export const hsla_string_to_rgb_browser = () => {
   const actual = hsl2rgb(hsl(0, 100, 50, 0.5));
   const expected = "rgba(255,0,0,0.5)";
+  return [expected, actual];
+};
+
+export const hsla_to_rgb_array_browser = () => {
+  const actual = hsl2rgb(0, 100, 50, 0.5, true);
+  const expected = [255,0,0,0.5];
   return [expected, actual];
 };
 
