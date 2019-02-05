90268bc066657e092ef2bf27d424588c455fe459
kristjanjansen <kristjan.jansen@gmail.com>
2019-02-05 18:07:41 +0200

    cleanup

--- src/vendor/vendor.js
+++ src/vendor/vendor.js
@@ -1,17 +1,30 @@
-import anime from 'animejs'
-import * as d3 from 'd3-shape'
-import chroma from 'chroma-js'
-import CodeMirror from 'codemirror';
-import colorBlind from 'color-blind'
-import gfm from 'codemirror/mode/gfm/gfm.js'
-import katex from 'katex';
-import marked from 'marked';
-import { require } from 'd3-require'
-import store from 'store/dist/store.modern.js'
-import THREE from 'three/build/three.js';
-//2.6+ import Vue from 'vue/dist/vue.esm.browser.js';
-import Vue from 'vue/dist/vue.js';
+import { require } from "d3-require";
+import * as d3 from "d3-shape";
+import anime from "animejs";
+import chroma from "chroma-js";
+import CodeMirror from "codemirror";
+import colorBlind from "color-blind";
+import gfm from "codemirror/mode/gfm/gfm.js";
+import katex from "katex";
+import marked from "marked";
+import store from "store/dist/store.modern.js";
+import THREE from "three/build/three.js";
+// 2.6+ import Vue from 'vue/dist/vue.esm.browser.js';
+import Vue from "vue/dist/vue.js";
 
-export { anime, d3, chroma, CodeMirror, colorBlind, gfm, katex, marked, require, store, THREE, Vue }
+export {
+  anime,
+  chroma,
+  CodeMirror,
+  colorBlind,
+  d3,
+  gfm,
+  katex,
+  marked,
+  require,
+  store,
+  THREE,
+  Vue
+};
 
-// Missing: store, aframe, prettier, anime
+// Missing: aframe, prettier
