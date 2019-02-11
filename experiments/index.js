d6b1348d5f12e8cfbf73fba2259d4f44bbbf7526
kristjanjansen <kristjan.jansen@gmail.com>
2019-02-11 11:11:13 +0200

    fachwerk() experiment

--- experiments/index.js
+++ experiments/index.js
@@ -1,14 +1,3 @@
-import { Vue,components, utils } from "../fachwerk.js";
+import { fachwerk } from './fachwerk.js'
 
-for (const name in components) {
-  Vue.component(name, components[name]);
-}
-
-Vue.prototype.$global = new Vue({ data: { state: {} } });
-new Vue({
-  el: "#app",
-  methods: { ...utils },
-  template: `
-  <f-scene grid><f-grid /></f-scene>
-  `
-});
+fachwerk({ editor: 'show' })
