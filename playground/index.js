51fae787222021c2cd54b7742bcf05675d5ea601
kristjanjansen <kristjan.jansen@gmail.com>
2019-03-04 15:08:56 +0000

    clean up sandbox

--- playground/index.js
+++ playground/index.js
@@ -1,21 +1,3 @@
 import { fachwerk } from "../src/fachwerk.js";
 
-import FFlower from "./FFlower.js"
-import FFlower2 from "./FFlower2.js"
-import FClock from "./FClock.js"
-import FPlaceholder from "./FPlaceholder.js"
-
-fachwerk({
-  components: { FFlower, FFlower2, FClock, FPlaceholder },
-  editor: "hide",
-  home: false,
-  pager: false,
-  type: 'document',
-  header: [
-    { title: "Scenarios", src: "https://designstem.github.io/scenarios" },
-    { title: "Project progress", src: "https://designstem.github.io/homepage" },
-    { title: "Framework", src: "https://designstem.github.io/homepage" },
-    { title: "Github", src: "https://designstem.github.io" }
-  ],
-  footer: true
-});
+fachwerk();
