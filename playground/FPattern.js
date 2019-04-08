1dd6ced8835f491644cd12fd14254e8630a41ff4
kristjanjansen <kristjan.jansen@gmail.com>
2019-04-08 09:59:21 +0300

    Work on hex grid

--- playground/FPattern.js
+++ playground/FPattern.js
@@ -9,14 +9,49 @@ export default {
     rows: { default: 3 },
     cols: { default: 3 },
   },
+  computed: {
+    xStep() {
+      return this.polarx(60,this.step) * 2
+    },
+    yStep() {
+      return this.step - this.polary(60,this.step)
+    }
+  },
   template: `
     <f-group>
       <f-group
-        v-for="(_, yIndex) in range(0, cols - 1)"
+        v-for="(_, yIndex) in range(0, rows - 1)"
         :key="yIndex"
       >
         <f-group
-          v-for="(_, xIndex) in range(0, yIndex % 2 ? rows : rows - 1)"
+          v-for="(_, xIndex) in range(0, cols - 1)"
+          :key="xIndex"
+          :position="[xIndex * xStep - (yIndex % 2 ? xStep / 2 : 0), yIndex * yStep]"
+        >
+          <slot />
+        </f-group>
+      </f-group>
+    </f-group>
+  `
+}
+
+export const A = {
+  methods: {
+    ...utils
+  },
+  props: {
+    step: { default: 1 },
+    rows: { default: 3 },
+    cols: { default: 3 },
+  },
+  template: `
+    <f-group :position="[(cols - 1 * step) / -2, (rows - 1 * step) / -2]">
+      <f-group
+        v-for="(_, yIndex) in range(0, rows - 1)"
+        :key="yIndex"
+      >
+        <f-group
+          v-for="(_, xIndex) in range(0, yIndex % 2 ? cols : cols - 1)"
           :key="xIndex"
           :position="[xIndex * step - (yIndex % 2 ? step / 2 : 0), yIndex * step]"
         >
@@ -26,7 +61,7 @@ export default {
     </f-group>
   `,
   template2: `
-  <f-group>
+  <f-group :position="[(cols - 1 * step) / -2, (rows - 1 * step) / -2]">
     <f-group
       v-for="(_, yIndex) in range(0, cols - 1)"
       :key="yIndex"
--- playground/index.md
+++ playground/index.md
@@ -1,7 +1,32 @@
 <f-scene grid>
+  <f-group scale="0.5">
+  <f-pattern>
+    <f-hexagon />
+  </f-pattern>
+  </f-group>
+</f-scene>
+
+
+
+<!--
+
+<f-scene grid>
+  <f-group scale="0.5">
+    <f-hexagon />
+    <f-point
+    	stroke-width="5"
+      stroke="red"
+      :x="polarx(60)"
+      :y="polary(60)"
+    />
+  </f-group>
+</f-scene>
+
+<f-scene grid>
 	<f-group scale="0.5">
   <f-pattern rows="3" cols="3" step="1" >
     <f-box />
   </f-pattern>
   </f-group>
 </f-scene>
+-->
