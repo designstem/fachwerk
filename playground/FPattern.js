9c1cc8bf76ba39a84c9198b2204e15c1a13e6a9d
kristjanjansen <kristjan.jansen@gmail.com>
2019-04-08 09:31:05 +0300

    Work on new pattern

--- playground/FPattern.js
+++ playground/FPattern.js
@@ -16,13 +16,29 @@ export default {
         :key="yIndex"
       >
         <f-group
-          v-for="(_, xIndex) in range(0, rows - 1)"
+          v-for="(_, xIndex) in range(0, yIndex % 2 ? rows : rows - 1)"
           :key="xIndex"
-          :position="[xIndex * step, yIndex * step]"
+          :position="[xIndex * step - (yIndex % 2 ? step / 2 : 0), yIndex * step]"
         >
           <slot />
         </f-group>
       </f-group>
     </f-group>
-  `
+  `,
+  template2: `
+  <f-group>
+    <f-group
+      v-for="(_, yIndex) in range(0, cols - 1)"
+      :key="yIndex"
+    >
+      <f-group
+        v-for="(_, xIndex) in range(0, rows - 1)"
+        :key="xIndex"
+        :position="[xIndex * step, yIndex * step]"
+      >
+        <slot />
+      </f-group>
+    </f-group>
+  </f-group>
+`
 }
--- playground/index.md
+++ playground/index.md
@@ -1,6 +1,6 @@
 <f-scene grid>
 	<f-group scale="0.5">
-  <f-pattern rows="4" cols="2" step="1" >
+  <f-pattern rows="3" cols="3" step="1" >
     <f-box />
   </f-pattern>
   </f-group>
