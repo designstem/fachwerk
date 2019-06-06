2b73bc937d046c1cc438a6964971d93a77101d2d
kristjanjansen <kristjan.jansen@gmail.com>
2019-06-06 10:09:41 +0300

    Improving docs

--- src/components/data/FFetch.js
+++ src/components/data/FFetch.js
@@ -2,6 +2,9 @@ export default {
   description: `
 Fetches data via AJAX.
 
+
+#### Fetching data from single local URL as text
+
 <f-fetch
   src="../README.md"
   v-slot="{ value }"
@@ -9,23 +12,37 @@ Fetches data via AJAX.
   <pre>{{ value.slice(0,100) }}</pre>
 </f-fetch>
 
-<!--f-fetch
+#### Fetching data from multple local URLs as text
+
+<f-fetch
+  :src="['../README.md','../RELEASES.md']"
+  v-slot="{ value }"
+>
+  <div>
+    <pre v-for="v in value">{{ v.slice(0,100) }}...</pre>
+  </div>
+</f-fetch>
+
+#### Fetching data from remote URL as JSON
+
+<f-fetch
   src="https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49"
   type="json"
   v-slot="{ value }"
 >
   <pre>{{ value.description }}</pre>
-</f-fetch-->
+</f-fetch>
   `,
   data: () => ({ value: null, loaded: false }),
   props: {
-    src: { default: "", type: String },
+    src: { default: "", type: [String,Array] },
     type: { default: "text", type: String }
   },
   slots: {
     value: {
       type: "string",
-      description: `Fetched contents. When \`type="json"\` is set, type can be \`array\` or \`object\``
+      description:
+        "Fetched contents as a `string`, `array` or `object`. When `src` is an array of multiple URLs, `value` is an `array` of `string`s, `array`s or `object`s"
     }
   },
   methods: {
@@ -39,12 +56,21 @@ Fetches data via AJAX.
     }
   },
   mounted() {
-    this.fetchType(this.src, this.type)
-      .then(res => {
-        this.value = res;
-        this.loaded = true;
-      })
-      .catch(error => console.log(error));
+    if (Array.isArray(this.src)) {
+      Promise.all(this.src.map(src => this.fetchType(src, this.type)))
+        .then(res => {
+          this.value = res;
+          this.loaded = true;
+        })
+        .catch(error => console.log(error));
+    } else {
+      this.fetchType(this.src, this.type)
+        .then(res => {
+          this.value = res;
+          this.loaded = true;
+        })
+        .catch(error => console.log(error));
+    }
   },
   render() {
     return this.loaded
