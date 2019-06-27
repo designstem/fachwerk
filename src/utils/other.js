0b3a0362ceb6c010e62584c7f76d3272548dfe8e
Andrus Naulainen <andus.naulainen@gmail.com>
2019-06-27 19:46:46 +0300

    Add snapcircle() and clean up snapToGrid()

--- src/utils/other.js
+++ src/utils/other.js
@@ -25,3 +25,31 @@ Logs a value from a template to the console
 `;
 
 export const log = value => console.log(value);
+
+export const snapgrid_help = () => `
+
+\`snapgrid(value, gridsize)\`
+
+Snaps object to grid
+
+`;
+
+export const snapgrid = (value, gridsize) => {
+  return value % gridsize < gridsize / 2
+      ? value - (value % gridsize)
+      : value + gridsize - (value % gridsize);
+};
+
+export const snapcircle_help = () => `
+
+\`snapcircle_help(value, gridsize)\`
+
+Snaps object to circle
+
+`;
+
+export const snapcircle = (value, radius, center) => {
+  // From https://math.stackexchange.com/questions/127613/closest-point-on-circle-edge-from-point-outside-inside-the-circle
+  return center + (radius * (value - center) / Math.sqrt(Math.pow(value - center, 2) + Math.pow(value - center, 2)));
+};
+
