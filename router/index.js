b08220f50be7bffd13dfc3e1ebe90ed496e9b033
kristjanjansen <kristjan.jansen@gmail.com>
2019-07-16 15:34:44 +0300

    Movie on

--- router/index.js
+++ router/index.js
@@ -1,4 +1,4 @@
-import { Vue, VueRouter, components, utils } from "../fachwerk.js";
+import { Vue, VueRouter, components, utils, kebabCase } from "../fachwerk.js";
 
 import menu from "../docs/menu.js";
 
@@ -18,7 +18,7 @@ const Hello = {
 `
 };
 
-const World = {
+const ComponentPage = {
   props: ["title", "a"],
   computed: {
     content() {
@@ -32,15 +32,85 @@ ${this.a}
 </div>`
 };
 
+const FilePage = {
+  props: ["title", "src"],
+  computed: {
+    content() {
+      return `# ${this.title}
+${this.a}
+    `;
+    }
+  },
+  template: `
+<f-fetch :src="src" v-slot="{ value: content }">
+<f-content-editor :content="content" />
+</f-fetch>`
+};
 const routes = [{ path: "/", component: Hello }];
 
-const menuRoutes = menu[3].items.slice(0, 10).map(c => ({
-  path: `/${c.component}`,
-  component: World,
-  props: { title: c.component, a: components[c.component].description }
-}));
+const slug = text => {
+  text = text.toString().toLowerCase().trim();
 
-console.log(menuRoutes);
+  const sets = [
+    {to: 'a', from: '[ÀÁÂÃÄÅÆĀĂĄẠẢẤẦẨẪẬẮẰẲẴẶ]'},
+    {to: 'c', from: '[ÇĆĈČ]'},
+    {to: 'd', from: '[ÐĎĐÞ]'},
+    {to: 'e', from: '[ÈÉÊËĒĔĖĘĚẸẺẼẾỀỂỄỆ]'},
+    {to: 'g', from: '[ĜĞĢǴ]'},
+    {to: 'h', from: '[ĤḦ]'},
+    {to: 'i', from: '[ÌÍÎÏĨĪĮİỈỊ]'},
+    {to: 'j', from: '[Ĵ]'},
+    {to: 'ij', from: '[Ĳ]'},
+    {to: 'k', from: '[Ķ]'},
+    {to: 'l', from: '[ĹĻĽŁ]'},
+    {to: 'm', from: '[Ḿ]'},
+    {to: 'n', from: '[ÑŃŅŇ]'},
+    {to: 'o', from: '[ÒÓÔÕÖØŌŎŐỌỎỐỒỔỖỘỚỜỞỠỢǪǬƠ]'},
+    {to: 'oe', from: '[Œ]'},
+    {to: 'p', from: '[ṕ]'},
+    {to: 'r', from: '[ŔŖŘ]'},
+    {to: 's', from: '[ßŚŜŞŠ]'},
+    {to: 't', from: '[ŢŤ]'},
+    {to: 'u', from: '[ÙÚÛÜŨŪŬŮŰŲỤỦỨỪỬỮỰƯ]'},
+    {to: 'w', from: '[ẂŴẀẄ]'},
+    {to: 'x', from: '[ẍ]'},
+    {to: 'y', from: '[ÝŶŸỲỴỶỸ]'},
+    {to: 'z', from: '[ŹŻŽ]'},
+    {to: '-', from: '[·/_,:;\']'}
+  ];
+
+  sets.forEach(set => {
+    text = text.replace(new RegExp(set.from,'gi'), set.to)
+  });
+
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/&/g, '-and-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\--+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '');
+}
+
+
+const menuMap = (c) => {
+  if (c.component) {
+    return {
+      path: `/${c.component}`,
+      component: ComponentPage,
+      props: { title: c.component, a: components[c.component].description }
+    }
+  }
+  if (c.file) {
+    return {
+      path: `/${slug(c.title)}`,
+      component: FilePage,
+      props: { title: c.title, src: c.file.replace('./','../docs/') }
+    }
+  }
+}
+
+const menuRoutes = menu[3].items.slice(-5).map(menuMap);
 
 const router = new VueRouter({
   // https://stackoverflow.com/questions/47677220/vuejs-history-mode-with-github-gitlab-pages
