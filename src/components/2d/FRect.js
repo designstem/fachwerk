53b150ddb4ae689beb4fa37ccf8e9ca71b9bb904
Andrus <andrus@sarponline.eu>
2019-07-17 16:19:43 +0300

    Add f-rect and remove points and r

---
+++ src/components/2d/FRect.js
@@ -0,0 +1,53 @@
+import Object2D from "./internal/Object2D.js";
+import { color, parseCoords } from "../../../fachwerk.js"
+;
+
+export default {
+  mixins: [Object2D],
+  description: `
+Displays a 2D rectangle.
+
+<f-scene grid>
+  <f-rect />
+  <f-rect
+    :stroke="color('red')"
+  />
+</f-scene>
+  `,
+  props: {
+    x: { default: 0, type: [Number, String] },
+    y: { default: 0, type: [Number, String] },
+    width: { default: 1, type: [Number, String] },
+    height: { default: 1, type: [Number, String] },
+    stroke: { default: "color('primary')", type: String },
+    strokeWidth: { default: 3, type: [Number, String] },
+    fill: { default: "none", type: String },
+    position: { default: "", type: [String, Number, Object, Array] },
+    rotation: { default: "", type: [String, Number, Object, Array] },
+    scale: { default: "1", type: [String, Number, Object, Array] },
+    opacity: { default: 1, type: [Number, String] }
+  },
+  computed: {
+    currentStrokeColor() {
+      return this.stroke == "color('primary')" ? color("primary") : this.stroke;
+    },
+    currentStrokeWidth() {
+      return this.strokeWidth * this.svgScale() * (1 / this.groupScale()) * (1 / this.scale)
+    },
+  },
+  template: `
+  <rect
+    :x="x"
+    :y="y"
+    :width="width"
+    :height="height"
+    :stroke="currentStrokeColor"
+    :stroke-width="currentStrokeWidth"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    :fill="fill"
+    :transform="transform"
+    :opacity="opacity"
+  />
+  `
+};
