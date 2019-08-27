0a4700c55866a53ea5b5bfc9309b0618c2b47010
kristjanjansen <kristjan.jansen@gmail.com>
2019-08-27 08:48:14 +0300

    Fixes

--- src/components/layout/FLayout.js
+++ src/components/layout/FLayout.js
@@ -3,44 +3,33 @@ import { get } from "../../../fachwerk.js";
 export default {
   props: {
     theme: { default: "light", type: String },
-    menu: { default: true, type: Boolean }
   },
+  data: () => ({
+    showMenu: true
+  }),
   methods: { get },
   template: `
     <f-theme :theme="theme" style="display: flex;">
-      <div style="
-        position: sticky;
-        min-width: 200px;
-        height: 100vh;
-        overflow: auto;
-        background: var(--background);
-        top: 0;
-      "
-        v-if="get('menu', true)"
+      <div
+        v-if="showMenu"
+        style="
+          position: sticky;
+          min-width: 200px;
+          height: 100vh;
+          overflow: auto;
+          background: var(--background);
+          top: 0;
+        "
+        @click="showMenu = false"
       >
         <slot name="menu_header" />
         <slot name="menu" />
       </div>
-      <div v-if="!get('menu', true)"
-        class="closedmenu"
-        style="
-          min-width: 40px;
-          height: 100vh;
-          position: sticky;
-          top: 0;
-          cursor: pointer;
-        "
-        @click="set('menu', true)"
-      >
-        <f-inline style="margin-top: var(--base); --inline-gap: 0; --inline-justify: center">
-          <a class="quaternary"><f-menu-icon /></a>
-        </f-inline>
-      </div>
       <div>
         <slot name="content" />
       </div>
-      <portal to="topright">
-        <slot name="topright" />
+      <portal to="topleft">
+        <a v-if="!showMenu" class="quaternary" @click="showMenu = true"><f-menu-icon /></a>
       </portal>
       <f-layer />
     </f-theme>
