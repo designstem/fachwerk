5816e7d108715bda66cf5575d6bc46eedf71dfb7
kristjanjansen <kristjan.jansen@gmail.com>
2019-10-14 11:16:10 +0300

    fixes

--- playground/index.js
+++ playground/index.js
@@ -8,7 +8,7 @@
 
 const sampleContent = `        
 | section: First section
-| background: red
+| theme: dark
 
 Hello world
 
@@ -16,6 +16,10 @@ Hello world
 
 for creating interactive learning materials in the browser. Content can be authored in a Markdown format, with custom additions such as dynamic layouts, interactivity and wide range of HTML-like components.
 
+
+---
+
+bla
 
 ---
 
@@ -31,79 +35,63 @@ import {
   parseContent,
   send,
   isimageurl,
-  color
+  color,
+  slug,
+  store
 } from "../fachwerk.js";
 
-export function fachwerk(c = {}) {
-  const config = {
-    el: "#fachwerk",
-    src: "./index.md",
-    title: "Fachwerk",
-    editor: "hide",
-    menu: "hide",
-    type: "document",
-    theme: "light",
-    style: {},
-    components: {},
-    utils: {},
-    ...c
-  };
-  for (const name in components) {
-    Vue.component(name, components[name]);
-  }
-  for (const name in config.components) {
-    Vue.component(name, config.components[name]);
-  }
-  Vue.mixin({ methods: { ...config.utils } });
-
-  Vue.config.productionTip = false;
-  Vue.prototype.$global = new Vue({ data: { state: {} } });
-
-  const FContentEditor2 = {
-    props: {
-      edit: { default: "hide", type: String },
-      menu: { default: "hide", type: String },
-      type: { default: "document", type: String }
-    },
-    data: () => ({
-      currentContent: "",
-      currentEdit: true,
-      currentMenu: false,
-      currentType: "slides"
-    }),
-    mounted() {
-      this.currentContent = sampleContent;
-      this.$global.$on("menu", () => {
-        this.currentMenu = !this.currentMenu;
-      });
-      this.$global.$on("edit", () => {
-        this.currentEdit = !this.currentEdit;
-      });
-      this.$global.$on("type", () => {
-        this.currentType =
-          this.currentType === "document" ? "slides" : "document";
-      });
-    },
-    computed: {
-      gridStyle() {
-        const editCol = this.currentEdit ? "1fr" : "";
-        const menubarCol = "55px";
-        const menuCol = this.currentMenu ? "200px" : "";
-        const contentCol = "1fr";
-        return [editCol, menubarCol, menuCol, contentCol].join(" ");
-      }
-    },
-    template: `
+const FContentEditor2 = {
+  props: {
+    edit: { default: "hide", type: String },
+    menu: { default: "hide", type: String },
+    type: { default: "document", type: String }
+  },
+  data: () => ({
+    currentContent: "",
+    currentEdit: true,
+    currentMenu: true,
+    currentType: "document"
+  }),
+  mounted() {
+    this.currentContent = sampleContent;
+    this.$global.$on("menu", () => {
+      this.currentMenu = !this.currentMenu;
+    });
+    this.$global.$on("edit", () => {
+      this.currentEdit = !this.currentEdit;
+    });
+    this.$global.$on("type", () => {
+      this.currentType =
+        this.currentType === "document" ? "slides" : "document";
+    });
+  },
+  computed: {
+    gridStyle() {
+      const editCol = this.currentEdit ? "1fr" : "";
+      const menubarCol = "55px";
+      const menuCol = this.currentMenu ? "200px" : "";
+      const contentCol = "1fr";
+      return [editCol, menubarCol, menuCol, contentCol].join(" ");
+    }
+  },
+  template: `
     <div class="grid" :style="{'--cols': gridStyle, '--gap': 0}">
-      <div v-if="currentEdit">
+      <div
+        v-if="currentEdit"
+        style="position: sticky; top: 0; height: 100vh;"
+      >
         <f-editor-header2 v-model="currentContent" />
         <f-advanced-editor v-model="currentContent" />
       </div>
-      <f-menubar2 :content="currentContent" />
+      <f-menubar2
+        :content="currentContent"
+        style="position: sticky; top: 0;"
+      />
       <f-menu2
         v-if="currentMenu"
         :menu="currentMenu"
         :content="currentContent"
+        style="position: sticky; top: 0;"
       />
       <div style="position: relative">
         <f-content2
@@ -123,13 +111,13 @@ currentType: {{ currentType }}
 gridStyle: {{ gridStyle }}</pre-->
     </div>
     `
-  };
+};
 
-  const FMenubar2 = {
-    props: {
-      menu: { default: false, type: Boolean }
-    },
-    template: `
+const FMenubar2 = {
+  props: {
+    menu: { default: false, type: Boolean }
+  },
+  template: `
     <div style="
       height: 100vh;
       padding: var(--base) 0;
@@ -163,20 +151,23 @@ gridStyle: {{ gridStyle }}</pre-->
       </div>
     </div>
     `
-  };
+};
 
-  const FMenu2 = {
-    props: {
-      content: { default: "", type: String },
-      menu: { default: false, type: Boolean }
-    },
-    data: () => ({ selected: null }),
-    computed: {
-      currentContent() {
-        return parseContent(this.content).filter(c => c.chapter || c.section);
-      }
-    },
-    template: `
+const FMenu2 = {
+  props: {
+    content: { default: "", type: String },
+    menu: { default: false, type: Boolean }
+  },
+  data: () => ({ currentSection: null }),
+  computed: {
+    currentContent() {
+      return parseContent(this.content).filter(c => c.chapter || c.section);
+    }
+  },
+  mounted() {
+    this.$global.$on("section", section => this.currentSection = section);
+  },
+  template: `
     <div v-if="menu" style="height: 100vh; padding: var(--base2) 0" >
       <div v-for="(c, i) in currentContent">
         <h5
@@ -197,11 +188,11 @@ gridStyle: {{ gridStyle }}</pre-->
             padding: var(--base) var(--base) var(--base) var(--base3);
             font-Weight: normal;
           "
-          @click="selected = c.section"
+          @click="currentSection = c.section; $global.$emit('section', c.section)"
         >
           <span :style="{
-            color: c.section == selected ? '' : 'var(--gray)',
-            borderBottom: c.section == selected ? '2px solid var(--blue)' : '',
+            color: c.section == currentSection ? '' : 'var(--gray)',
+            borderBottom: c.section == currentSection ? '2px solid var(--blue)' : '',
           }">
           {{ c.section }}
           </span>
@@ -209,23 +200,13 @@ gridStyle: {{ gridStyle }}</pre-->
       </div>
     </div>
     `
-  };
+};
 
-  /*
-
-          :style="{
-            color: get('section','') == c.section ? 'var(--background)' : 'var(--primary)',
-            background: get('section','') == c.section ? 'var(--lightergray)' : 'var(--transparent)',
-          }"
-          @click="set('section', c.section); goto(c.section)"
-
-  */
-
-  const FEditorHeader2 = {
-    props: {
-      value: { default: "", type: String }
-    },
-    template: `
+const FEditorHeader2 = {
+  props: {
+    value: { default: "", type: String }
+  },
+  template: `
     <div style="
       height: var(--base6);
       padding: 0 var(--base);
@@ -238,29 +219,82 @@ gridStyle: {{ gridStyle }}</pre-->
       <a class="quaternary" @click="$global.$emit('edit')"><f-close-icon /></a>
     </div>
     `
-  };
+};
 
-  const FPager2 = {
-    methods: { send },
-    template: `
+const FPager2 = {
+  methods: { send },
+  template: `
   <div style="display: flex;">
     <a class="quaternary" style="padding: 0 4px" @click="send('prev')" ><f-leftarrow-icon /></a>
     <a class="quaternary" style="padding: 0 4px" @click="send('next')" ><f-rightarrow-icon /></a>
   </div>
   `
-  };
+};
 
-  const FContentHeader2 = {
-    props: {
-      content: { default: "", type: String },
-      type: { default: "document", type: String }
+const FContentHeader2 = {
+  props: {
+    content: { default: "", type: String },
+    type: { default: "document", type: String },
+    saveId: { default: "fachwerk", type: String }
+  },
+  data: () => ({ currentIndex: 0 }),
+  computed: {
+    currentContent() {
+      return parseContent(this.content);
+    }
+  },
+  methods: {
+    first() {
+      this.currentIndex = 0;
+      this.$global.$emit("index", this.currentIndex);
     },
-    computed: {
-      currentContent() {
-        return parseContent(this.content);
+    last() {
+      this.currentIndex = this.currentContent.length - 1;
+      this.$global.$emit("index", index);
+    },
+    prev() {
+      if (this.currentIndex > 0) {
+        this.currentIndex--;
+        this.$global.$emit("index", this.currentIndex);
       }
     },
-    template: `
+    next() {
+      if (this.currentIndex < this.currentContent.length - 1) {
+        this.currentIndex++;
+        this.$global.$emit("index", this.currentIndex);
+      }
+    },
+    goto(id) {
+      if (typeof id === "string") {
+        const index = this.currentContent.findIndex(
+          slide => slide.section === id || slide.id === id
+        );
+        if (index > -1) {
+          this.currentIndex = index;
+          window.location.hash = slug(id);
+        }
+      } else {
+        this.currentIndex = id;
+        window.location.hash = "id-" + i;
+      }
+      this.$global.$emit("index", this.currentIndex);
+    }
+  },
+  mounted() {
+    this.$global.$on("section", section => this.goto(section));
+    this.$watch(
+      "currentIndex",
+      currentIndex => {
+        const currentSlide = this.currentContent[currentIndex];
+        if (currentSlide && currentSlide.section) {
+          this.$global.$emit("section", currentSlide.section);
+        }
+        store.set(this.saveId + ".index", currentIndex);
+      },
+      { immediate: true }
+    );
+  },
+  template: `
     <div style="
       position: absolute;
       top: 0;
@@ -272,73 +306,80 @@ gridStyle: {{ gridStyle }}</pre-->
       align-items: center;
       justify-content: space-between;
     ">
-      &nbsp;
-      <f-pager2 v-if="type == 'slides' && currentContent.length > 1" />
+      <pre>{{ currentIndex }}</pre>
+      <div v-if="type == 'slides' && currentContent.length > 1" style="display: flex;">
+        <a class="quaternary" style="padding: 0 4px" @click="prev" ><f-leftarrow-icon /></a>
+        <a class="quaternary" style="padding: 0 4px" @click="next" ><f-rightarrow-icon /></a>
+      </div>
     </div>
     `
-  };
+};
 
-  const FContent2 = {
-    props: {
-      content: { default: "", type: String },
-      type: { default: "document", type: String }
+const FContent2 = {
+  props: {
+    content: { default: "", type: String },
+    type: { default: "document", type: String }
+  },
+  data: () => ({
+    currentIndex: 0
+  }),
+  computed: {
+    currentContent() {
+      return parseContent(this.content);
+    }
+  },methods: {
+    slug,
+    gridStyle(slide) {
+      return {
+        display: "grid",
+        gridTemplateColumns: slide.cols
+          ? slide.cols
+          : "repeat(" + slide.colCount + ", 1fr)",
+        gridTemplateRows: slide.rows
+          ? slide.rows
+          : "repeat(" + slide.rowCount + ", auto)",
+        gridTemplateAreas: slide.areas,
+        gridGap: slide.gap ? slide.gap : "var(--gap)"
+      };
     },
-    data: () => ({
-      currentIndex: 0
-    }),
-    computed: {
-      currentContent() {
-        return parseContent(this.content);
-      }
-    },
-    methods: {
-      gridStyle(slide) {
-        return {
-          display: "grid",
-          gridTemplateColumns: slide.cols
-            ? slide.cols
-            : "repeat(" + slide.colCount + ", 1fr)",
-          gridTemplateRows: slide.rows
-            ? slide.rows
-            : "repeat(" + slide.rowCount + ", auto)",
-          gridTemplateAreas: slide.areas,
-          gridGap: slide.gap ? slide.gap : "var(--gap)"
-        };
-      },
-      backgroundStyle(slide) {
-        const tint = slide.tint ? slide.tint : 0.3;
-        const background = slide.background
-          ? isimageurl(slide.background)
-            ? `linear-gradient(
+    backgroundStyle(slide) {
+      const tint = slide.tint ? slide.tint : 0.3;
+      const background = slide.background
+        ? isimageurl(slide.background)
+          ? `linear-gradient(
                 rgba(0, 0, 0, ${tint}),
                 rgba(0, 0, 0, ${tint})
                 ),
                   url(${slide.background})
               `
-            : color(slide.background)
-          : "";
+          : color(slide.background)
+        : "";
 
-        return {
-          background,
-          backgroundSize: slide.background ? "cover" : "",
-          backgroundRepeat: slide.background ? "no-repeat" : ""
-        };
-      }
-    },
-    mounted() {
-      this.$global.$on("next", () => this.currentIndex++);
-      this.$global.$on("prev", () => this.currentIndex--);
-    },
-    template: `
-    <div :style="{
-      display: type == 'document' ? 'flex' : 'block',
-      justifyContent: 'center'
-    }">
+      return {
+        background,
+        backgroundSize: slide.background ? "cover" : "",
+        backgroundRepeat: slide.background ? "no-repeat" : ""
+      };
+    }
+  },
+  mounted() {
+    this.$global.$on("index", index => (this.currentIndex = index));
+  },
+  template: `
+    <f-theme
+      theme="light"
+      :style="{
+        display: type == 'document' ? 'flex' : 'block',
+        justifyContent: 'center'
+      }"
+    >
       <div>
         <div
+          v-if="type == 'slides' ? i == currentIndex : true"
           v-for="(slide,i) in currentContent"
           :key="i"
-          v-if="type == 'slides' ? i == currentIndex : true"
+          :id="slide.section ? slug(slide.section) : 'id-' + i"
+          :theme="slide.theme ? slide.theme : ''"
           :style="{
             ...gridStyle(slide),
             ...backgroundStyle(slide),
@@ -358,22 +399,44 @@ gridStyle: {{ gridStyle }}</pre-->
           />
         </div>
       </div>
-    </div>
+    </f-theme>
     `
-  };
+};
 
-  Vue.component("FContentEditor2", FContentEditor2);
-  Vue.component("FMenubar2", FMenubar2);
-  Vue.component("FMenu2", FMenu2);
-  Vue.component("FEditorHeader2", FEditorHeader2);
-  Vue.component("FContent2", FContent2);
-  Vue.component("FContentHeader2", FContentHeader2);
-  Vue.component("FPager2", FPager2);
+Vue.component("FContentEditor2", FContentEditor2);
+Vue.component("FMenubar2", FMenubar2);
+Vue.component("FMenu2", FMenu2);
+Vue.component("FEditorHeader2", FEditorHeader2);
+Vue.component("FContent2", FContent2);
+Vue.component("FContentHeader2", FContentHeader2);
+Vue.component("FPager2", FPager2);
+
+export function fachwerk2(c = {}) {
+  const config = {
+    el: "#fachwerk",
+    src: "./index.md",
+    title: "Fachwerk",
+    editor: "hide",
+    menu: "hide",
+    type: "document",
+    theme: "light",
+    style: {},
+    components: {},
+    utils: {},
+    ...c
+  };
+  for (const name in components) {
+    Vue.component(name, components[name]);
+  }
+  for (const name in config.components) {
+    Vue.component(name, config.components[name]);
+  }
+  Vue.mixin({ methods: { ...config.utils } });
+
+  Vue.config.productionTip = false;
+  Vue.prototype.$global = new Vue({ data: { state: {} } });
 
   new Vue({
-    components: {
-      FContentEditor2
-    },
     el: config.el,
     data: {
       config
@@ -388,4 +451,4 @@ gridStyle: {{ gridStyle }}</pre-->
   });
 }
 
-fachwerk();
+fachwerk2();
