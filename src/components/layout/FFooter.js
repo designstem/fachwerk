b9f3d44eaa710bbb2b71988601f24c754292731d
kristjanjansen <kristjan.jansen@gmail.com>
2019-10-16 16:49:39 +0300

    footer br

--- src/components/layout/FFooter.js
+++ src/components/layout/FFooter.js
@@ -9,7 +9,7 @@ Displays a page footer.
   template: `
   <div class="grid" style="--cols: auto 1fr auto">
     <p style="opacity: 0.5">
-      All code is licenced under <a href="https://choosealicense.com/licenses/mit/" rel="licence">MIT licence</a>. All content is licenced under Creative Commons
+      All code is licenced under <a href="https://choosealicense.com/licenses/mit/" rel="licence">MIT licence</a>.<br>All content is licenced under Creative Commons
       <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">BY-NC-SA 4.0</a> International License.
     </p>
     &nbsp;
